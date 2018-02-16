b3cbabc7dc8a69ab30cdddcf991bf3ab0b8e3b25
Sweeper777 <sumulang@gmail.com>
2018-02-16 09:11:21 +0000

    set up scene and console

--- SceneKitConsole/ViewController.swift
+++ SceneKitConsole/ViewController.swift
@@ -7,11 +7,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var sceneView: SCNView!
     
     var console: SceneKitConsole!
+    let scene = MyScene()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         textField.delegate = self
-        console = SceneKitConsole(attachTo: sceneView)
+        console = SceneKitConsole(attachTo: sceneView, commandProvider: CommandProvider.Builder().addStandardCommands().build())
+        scene.setup()
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
